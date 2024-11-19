43f04392f79faacb972efff03eda752891f8bb30
venkatasai-03 <141182370+venkatasai-03@users.noreply.github.com>
2024-11-19 15:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def index():
     return render_template('index.html', crop=None, quality=None, image_url=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
